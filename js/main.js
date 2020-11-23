794a82d8eee8f05d35cf66053f0c637ab4d85ba7
Angelita Santos <65367322+angelitasantos@users.noreply.github.com>
2020-11-23 18:15:39 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,69 @@
+// ---------------------------------------------------------------------------------- //
+// -------------------------- Títulos, Subtítulos e Textos -------------------------- //
+// ---------------------------------------------------------------------------------- //
+
+
+var textoPrincipal = "Serviços especializados em gestão financeira, administrativa e operacional."
+$("#textoPrincipal").html(textoPrincipal);
+
+var btnInfo = "Mais informações"
+$(".btnInfo").html(btnInfo);
+
+var btnSaibaMais = "Saiba Mais"
+$(".btnSaibaMais").html(btnSaibaMais);
+
+var linkInfo = "Continue lendo ..."
+$(".linkInfo").html(linkInfo);
+
+var emConstrucao = "Em Desenvolvimento"
+$(".emConstrucao").html(emConstrucao);
+
+var textoAreaRestrita = "Área Restrita"
+$(".textoAreaRestrita").html(textoAreaRestrita);
+
+var planoBasico = "Quer ter o controle de suas atividades na palma da mão?"
+$(".planoBasico").html(planoBasico);
+
+var planoPrata = "Há quanto tempo você trabalha mais de 12 horas por dia?"
+$(".planoPrata").html(planoPrata);
+
+var planoOuro = "O que está faltando para os seus sonhos se tornarem realidade? "
+$(".planoOuro").html(planoOuro);
+
+
+
+var textoQuemSomos = "Em uma economia competitiva e dinâmica, é cada vez mais importante que a Gestão de um negócio se adapte às novas realidades exigidas pelo mercado e pelos consumidores. Oferecemos serviços especializados em gestão empresarial para empreendedores, artesãos, produtores e pequenas empresas. Nossa equipe atua com os empreendedores, a fim de identificar os 6 pilares ligados a melhoria dos resultados financeiros, operacionais e comerciais: pessoas, métodos, processos, planejamento, indicadores, tecnologia."
+$(".textoQuemSomos").html(textoQuemSomos);
+
+
+
+var textoFinanceiro = "Auxiliar a reduzir despesas financeiras, melhorar o retorno sobre o patrimônio, obter ganhos no fluxo de caixa operacional e eliminar desperdícios."
+$(".textoFinanceiro").html(textoFinanceiro);
+
+var textoVendas = "Assegurar o crescimento das vendas, a melhoria da lucratividade e a longevidade dos resultados.  Identificar e padronizar as melhores práticas."
+$(".textoVendas").html(textoVendas);
+
+var textoEstoque = "Equilibrar compras, armazenagem e entregas, controlando as entradas e o consumo de materiais e movimentando o ciclo dos produtos."
+$(".textoEstoque").html(textoEstoque);
+
+
+
+var textoCompras = "A otimização dos custos e o potencial aumento da lucratividade sobre as vendas atraves da padronização dos produtos do planejamento das compras com base no perfil dos seus clientes."
+$(".textoCompras").html(textoCompras);
+
+var textoProducao = "Levantar e analisar as interrupções nos processos produtivos e as respectivas causas dessas interrupções, viabilizando um maior ganho de produtividade."
+$(".textoProducao").html(textoProducao);
+
+var textoAdministrativo = "Gerenciar as rotinas a fim de permitir que profissionais sejam capazes de resolver problemas do dia a dia e de minimizar a sobrecarga de supervisores e gerentes."
+$(".textoAdministrativo").html(textoAdministrativo);
+
+
+
+var artigoFinanceiro = "Maior eficiência do processo orçamentário e de controle dos gastos e crescimento da receita operacional, com melhoria da margem de contribuição."
+$(".artigoFinanceiro").html(artigoFinanceiro);
+
+var artigoProcesso = "Padronização dos procedimentos gerenciais e operacionais com aumento da produtividade e garantia de processos estáveis e padronizados."
+$(".artigoProcesso").html(artigoProcesso);
+
+var artigoEstrutura = "Foco nos resultados de médio e longo prazo e maximização do lucro e do crescimento sustentado. Aumento da confiabilidade nos serviços e produtos."
+$(".artigoEstrutura").html(artigoEstrutura);
